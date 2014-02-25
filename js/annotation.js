95ddc33f1120f8c15681c57defafa771a4e6578d
elju <moresilenter@gmail.com>
2014-02-25 01:59:16 -0500

    temp commit

--- js/annotation.js
+++ js/annotation.js
@@ -1,17 +1,33 @@
-veryDRYFunction = function() {
-  $('article .annotation').css({
-      top: $('.annotation-one').position().top
+veryDRYFunction = function($span, $annotation) {
+  $annotation.css({
+      top: $span.position().top
   });
 };
 
-$(document).ready(veryDRYFunction);
+veryDRYWrapper = function() {
+  $('.article-annotation').each(function() {
+    var $this = $(this);
+    veryDRYFunction($this, $this.next());
+  });
+};
 
-$(window).resize(veryDRYFunction);
+$(document).ready(veryDRYWrapper);
+
+$(window).resize(veryDRYWrapper);
 
 $('a[href^="annotation://"]').each(function(i) {
   var $this = $(this);
   var text = $this.text();
-                                               ////////////////////////////////////
-  var href = $this.attr('href').substring(13); // 13 = length of "annotation://" //
-                                               ////////////////////////////////////
-  $('
+                                                           ////////////////////////////////////
+  var annotation_text = $this.attr('href').substring(13); // 13 = length of "annotation://" //
+                                                         ////////////////////////////////////
+  var $span = $('<span class="article-annotation">').text(text).tooltip({
+    title: annotation_text
+  });
+  var $annotation = $('<span class="annotation">').text(annotation_text);
+
+  $span.insertBefore($this);
+  $annotation.insertAfter($this);
+  $this.remove();
+  veryDRYFunction($span, $annotation);
+});
